7afb9b307da78b9b84b68091434dd2a603a5f0d4
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2025-07-02 09:45:32 +0000

    Add style, center in Zaragoza, add hiking route

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ import * as maplibregl from "https://esm.sh/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'map', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    style: './assets/style.json', // style URL
+    // style: 'https://demotiles.maplibre.org/style.json', // alternative
+    center: [41.65, -0.88], // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
+
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
